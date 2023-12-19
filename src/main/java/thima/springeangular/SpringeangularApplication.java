b15be063169e2ae4dfaf573cb67599d75103e32b
Thimachal <thimachal@gmail.com>
2023-12-18 21:54:02 -0300

    first commit spring and angular

---
+++ src/main/java/thima/springeangular/SpringeangularApplication.java
@@ -0,0 +1,13 @@
+package thima.springeangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringeangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringeangularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thima/springeangular/SpringeangularApplicationTests.java
@@ -0,0 +1,13 @@
+package thima.springeangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringeangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
