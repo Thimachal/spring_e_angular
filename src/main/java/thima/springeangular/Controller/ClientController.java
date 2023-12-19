58bf4247df005a9dba9b0916bb4aae6b4ddb8fb2
Thimachal <thimachal@gmail.com>
2023-12-19 10:58:26 -0300

    SpringeAngularInit

--- src/main/java/thima/springeangular/Controller/ClientController.java
+++ src/main/java/thima/springeangular/Controller/ClientController.java
@@ -1,8 +1,13 @@
 package thima.springeangular.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import thima.springeangular.Model.Client;
 import thima.springeangular.Service.ClientService;
 
 @RestController("/")
@@ -12,8 +17,18 @@ public class ClientController {
     private ClientService actionService;
 
         @GetMapping("/all")
-        public String tste(){
+        public String teste(){
             return ("thats ok");
+        }
+        @PostMapping("/registerClient")
+    public ResponseEntity<?> register(@RequestBody Client clientFromBodyFront) {
+
+            try {
+                ResponseEntity<?> clientFromService = actionService.registerClient(clientFromBodyFront);
+                return new ResponseEntity<>(clientFromService.getBody(), HttpStatus.CREATED);
+            } catch (RuntimeException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
         }
 
 }
