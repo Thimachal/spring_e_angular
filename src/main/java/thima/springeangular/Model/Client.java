b893a0ad2f3bac72c8d991aaf25c12b544d540de
Thimachal <thimachal@gmail.com>
2023-12-19 10:35:02 -0300

    SpringeAngularInit

--- src/main/java/thima/springeangular/Model/Client.java
+++ src/main/java/thima/springeangular/Model/Client.java
@@ -9,4 +9,49 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long clientID;
     private String clientName;
+
+    private Integer clientAge;
+    private String clientCity;
+
+    public Client() {
+    }
+
+    public Client(Long clientID, String clientName, Integer clientAge, String clientCity) {
+        this.clientID = clientID;
+        this.clientName = clientName;
+        this.clientAge = clientAge;
+        this.clientCity = clientCity;
+    }
+
+    public Long getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(Long clientID) {
+        this.clientID = clientID;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public Integer getClientAge() {
+        return clientAge;
+    }
+
+    public void setClientAge(Integer clientAge) {
+        this.clientAge = clientAge;
+    }
+
+    public String getClientCity() {
+        return clientCity;
+    }
+
+    public void setClientCity(String clientCity) {
+        this.clientCity = clientCity;
+    }
 }
