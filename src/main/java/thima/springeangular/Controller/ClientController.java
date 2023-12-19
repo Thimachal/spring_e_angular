6ec1778169ca8014b8b86d1a429ed6adc55d7921
Thimachal <thimachal@gmail.com>
2023-12-19 10:45:27 -0300

    SpringeAngularInit

--- src/main/java/thima/springeangular/Controller/ClientController.java
+++ src/main/java/thima/springeangular/Controller/ClientController.java
@@ -1,10 +1,15 @@
 package thima.springeangular.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import thima.springeangular.Service.ClientService;
 
 @RestController("/")
 public class ClientController {
+
+    @Autowired
+    private ClientService actionService;
 
         @GetMapping("/all")
         public String tste(){
