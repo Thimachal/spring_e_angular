550571a1517f0548284bb2921c35216a5b9ef89d
Thimachal <thimachal@gmail.com>
2023-12-18 22:10:02 -0300

    SpringeAngularInit

--- src/main/java/thima/springeangular/Controller/ClientController.java
+++ src/main/java/thima/springeangular/Controller/ClientController.java
@@ -1,4 +1,14 @@
 package thima.springeangular.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
 public class ClientController {
+
+        @GetMapping("/all")
+        public String tste(){
+            return ("thats ok");
+        }
+
 }
