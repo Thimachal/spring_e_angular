e6f9f8410377dccd671be360d935ae838c18bf05
Thimachal <thimachal@gmail.com>
2023-12-19 10:43:29 -0300

    SpringeAngularInit

--- src/main/java/thima/springeangular/Repository/ClientRepository.java
+++ src/main/java/thima/springeangular/Repository/ClientRepository.java
@@ -1,7 +1,9 @@
 package thima.springeangular.Repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import thima.springeangular.Model.Client;
-
+@Repository
 public interface ClientRepository extends CrudRepository<Client,Long> {
+
 }
