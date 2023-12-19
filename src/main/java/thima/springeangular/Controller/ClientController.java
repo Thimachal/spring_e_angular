eecfc1b6dbafaec0898a79fae2d7026708d7b3a1
Thimachal <thimachal@gmail.com>
2023-12-19 11:10:16 -0300

    SpringeAngularInit

--- src/main/java/thima/springeangular/Controller/ClientController.java
+++ src/main/java/thima/springeangular/Controller/ClientController.java
@@ -3,22 +3,25 @@ package thima.springeangular.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import thima.springeangular.Model.Client;
 import thima.springeangular.Service.ClientService;
 
-@RestController("/")
+@RestController()
+@RequestMapping("/clients")
 public class ClientController {
 
     @Autowired
     private ClientService actionService;
 
-        @GetMapping("/all")
-        public String teste(){
-            return ("thats ok");
+        @GetMapping("/allClients")
+        public ResponseEntity<?> getAllClients(){
+            try {
+                return new ResponseEntity<>(actionService.listAllClients().getBody(),HttpStatus.OK);
+            }catch (RuntimeException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
         }
         @PostMapping("/registerClient")
     public ResponseEntity<?> register(@RequestBody Client clientFromBodyFront) {
