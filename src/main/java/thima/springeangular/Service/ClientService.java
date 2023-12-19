f874fe3c94b76a826b2d20208287ebe956e56bee
Thimachal <thimachal@gmail.com>
2023-12-19 10:58:38 -0300

    SpringeAngularInit

--- src/main/java/thima/springeangular/Service/ClientService.java
+++ src/main/java/thima/springeangular/Service/ClientService.java
@@ -1,7 +1,20 @@
 package thima.springeangular.Service;
 
+import org.modelmapper.internal.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import thima.springeangular.Model.Client;
+import thima.springeangular.Repository.ClientRepository;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository actionRepository;
+    public ResponseEntity<?> registerClient(Client clientFromBodyFront) {
+    Client client = new Client();
+       client = actionRepository.save(clientFromBodyFront);
+        return ResponseEntity.ok(client);
+    }
 }
